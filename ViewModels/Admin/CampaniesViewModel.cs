054a63120d7a2a597aecabc95424ed754ff09f8c
Frédéric Carisey <63855888+fcarisey@users.noreply.github.com>
2025-05-16 17:12:23 +0200

    Ajout de la gestion des entreprises
    Implémentation des fonctionnalités pour ajouter, modifier et supprimer des entreprises dans les fichiers `CampaniesViewModel.cs`, `AddViewModel.cs`, et `ModifyViewModel.cs`.
    Mise à jour des vues XAML (`AddView.xaml`, `ModifyView.xaml`, `CompaniesView.xaml`) pour intégrer les nouvelles propriétés et commandes.
    Amélioration de l'interface utilisateur avec des ajustements de mise en page et des éléments interactifs.
    Modification de `ModifyView.xaml.cs` pour gérer la navigation avec des paramètres.

--- ViewModels/Admin/CampaniesViewModel.cs
+++ ViewModels/Admin/CampaniesViewModel.cs
@@ -1,11 +1,99 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using StageTracker.Interfaces.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
 
 namespace StageTracker.ViewModels.Admin;
 
 public partial class CampaniesViewModel : BaseViewModel
 {
+    private readonly ObservableCollection<Models.Company> _companies = [];
+
+    [ObservableProperty]
+    private ICollectionView _filteredCompanies;
+
+    private readonly INavigationService _navigationService;
+
+    public CampaniesViewModel(INavigationService navigationService)
+    {
+        _companies =
+        [
+            new() {Id = 1, Name = "Google", Address = "1600 Amphitheatre Parkway, Mountain View, CA 94043", PhoneNumber = "+1 650-253-0000", Email = "google@gmail.com", Website = "https://www.google.com"},
+            new() {Id = 2, Name = "Microsoft", Address = "1 Microsoft Way, Redmond, WA 98052", PhoneNumber = "+1 425-882-8080", Email = "microsoft@gmail.com", Website = "https://www.microsoft.com"},
+            new() {Id = 3, Name = "Apple", Address = "1 Apple Park Way, Cupertino, CA 95014", PhoneNumber = "+1 408-996-1010", Email = "apple@gmail.com", Website = "https://www.apple.com"},
+            new() {Id = 4, Name = "Amazon", Address = "410 Terry Ave N, Seattle, WA 98109", PhoneNumber = "+1 206-266-1000", Email = "amazon@gmail.com", Website = "https://www.amazon.com"},
+            new() {Id = 5, Name = "Facebook", Address = "1 Hacker Way, Menlo Park, CA 94025", PhoneNumber = "+1 650-543-4800", Email = "facebook@gmail.com", Website = "https://www.facebook.com"},
+            new() {Id = 6, Name = "Netflix", Address = "100 Winchester Circle, Los Gatos, CA 95032", PhoneNumber = "+1 408-540-3700", Email = "netflix@gmail.com", Website = "https://www.netflix.com"},
+            new() {Id = 7, Name = "Tesla", Address = "3500 Deer Creek Road, Palo Alto, CA 94304", PhoneNumber = "+1 650-681-5000", Email = "tesla@gmail.com", Website = "https://www.tesla.com"},
+            new() {Id = 8, Name = "SpaceX", Address = "1 Rocket Road, Hawthorne, CA 90250", PhoneNumber = "+1 310-363-6000", Email = "spacex@gmail.com", Website = "https://www.spacex.com"},
+            new() {Id = 9, Name = "IBM", Address = "1 New Orchard Road, Armonk, NY 10504", PhoneNumber = "+1 914-499-1900", Email = "ibm@gmail.com", Website = "https://www.ibm.com"},
+            new() {Id = 10, Name = "Intel", Address = "2200 Mission College Blvd, Santa Clara, CA 95054", PhoneNumber = "+1 408-765-8080", Email = "intel@gmail.com", Website = "https://www.intel.com"},
+        ];
+
+        _filteredCompanies = CollectionViewSource.GetDefaultView(_companies);
+        _navigationService = navigationService;
+    }
+
+    [RelayCommand]
+    public void OnTextChanged(string searchTerms)
+    {
+        if (!string.IsNullOrEmpty(searchTerms) && searchTerms.Length > 0)
+        {
+            FilteredCompanies.Filter = x =>
+            {
+                if (x is Models.Company company)
+                {
+                    if (company != null)
+                    {
+                        return company.Name.Contains(searchTerms, StringComparison.CurrentCultureIgnoreCase) ||
+                               company.Email.Contains(searchTerms, StringComparison.CurrentCultureIgnoreCase) ||
+                               company.PhoneNumber.Contains(searchTerms, StringComparison.CurrentCultureIgnoreCase) ||
+                               company.Address.Contains(searchTerms, StringComparison.CurrentCultureIgnoreCase);
+                    }
+                }
+
+                return false;
+            };
+
+            FilteredCompanies.Refresh();
+        }
+        else
+        {
+            FilteredCompanies.Filter = null;
+            FilteredCompanies.Refresh();
+        }
+    }
+
+    [RelayCommand]
+    public void AddCompany()
+    {
+        _navigationService.NavigateTo<Views.Admin.Company.AddView>();
+    }
+
+    [RelayCommand]
+    public void ModifyCompany(Models.Company company)
+    {
+        _navigationService.NavigateTo<Views.Admin.Company.ModifyView>(company);
+    }
+
+    [RelayCommand]
+    public void DeleteCompany(Models.Company company)
+    {
+        MessageBoxResult result = MessageBox.Show($"Voulez-vous vraiment supprimer {company.Name}", "Suppression de l'entreprise", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+        if (result == MessageBoxResult.Yes)
+        {
+            // Supression de la bdd
+            _companies.Remove(company);
+            FilteredCompanies.Refresh();
+        }
+    }
 }
--- ViewModels/Admin/Company/ModifyViewModel.cs
+++ ViewModels/Admin/Company/ModifyViewModel.cs
@@ -1,11 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using StageTracker.Interfaces.Services;
+using StageTracker.Interfaces.ViewModels;
+using System.Windows;
 
 namespace StageTracker.ViewModels.Admin.Company;
 
-public partial class ModifyViewModel : BaseViewModel
+public partial class ModifyViewModel : BaseViewModel, INavigableWithParameter
 {
+    private readonly INavigationService _navigationService;
+
+    [ObservableProperty]
+    private Models.Company? _company;
+
+    public ModifyViewModel(INavigationService navigationService)
+    {
+        _navigationService = navigationService;
+    }
+
+    public void OnNavigatedTo(object parameter)
+    {
+        if (parameter is Models.Company company)
+        {
+            Company = company;
+        }
+    }
+
+    [RelayCommand]
+    private void ModifyCompany()
+    {
+        // Créer un nouvel enseignant dans la base de données
+        MessageBox.Show($"Company modifié avec succès ! {Company?.Name} - {Company?.Email} - {Company?.Website}", "Création d'entreprise", MessageBoxButton.OK, MessageBoxImage.Information);
+        _navigationService.NavigateTo<Views.Admin.CompaniesView>();
+    }
 }
