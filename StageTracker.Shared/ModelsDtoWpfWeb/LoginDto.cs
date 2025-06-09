2d0376b51fc4385add91059f9b02ecc3cc3e8f42
Frédéric Carisey <63855888+fcarisey@users.noreply.github.com>
2025-06-09 18:24:59 +0200

    Mise à jour de l'application avec nouvelles fonctionnalités
    Cette mise à jour inclut l'ajout de Blazored.LocalStorage pour la gestion du stockage local. La structure de la navigation a été améliorée dans `MainLayout.razor` et `StudentNavMenu.razor`, avec des liens vers les pages "Offres" et "Candidatures".
    
    Le modèle de connexion `LoginDto` a été créé avec des validations, et un nouveau formulaire de connexion a été ajouté dans `Login.razor`. Les fichiers `Applications.razor` et `Offers.razor` ont été créés pour une future fonctionnalité.
    
    La bibliothèque Bootstrap a été mise à jour de `5.1.0` à `5.3.6`, et la langue de l'application a été changée en français. Des ajustements CSS ont été effectués pour améliorer l'esthétique générale et l'expérience utilisateur.

---
+++ StageTracker.Shared/ModelsDtoWpfWeb/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StageTracker.Shared.ModelsDtoWpfWeb;
+
+public record class LoginDto()
+{
+    [Required(ErrorMessage = "Une adresse mail est requise !")]
+    [EmailAddress(ErrorMessage = "L'adresse ne correspond pas !")]
+    public string? Email { get; set; }
+
+    [Required(ErrorMessage = "Un mot de passe est requis !")]
+    //[MinLength(12, ErrorMessage = "Le mot de passe ne correspond pas !")] dev, bypass password
+    public string? Password { get; set; }
+}
