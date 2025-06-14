a1dc877c5475d6e679295d1c51f9c97ea8dfe352
Frédéric Carisey <63855888+fcarisey@users.noreply.github.com>
2025-06-14 22:49:10 +0200

    Ajout de la gestion des rôles et refonte des pages
    - Implémentation d'une logique d'initialisation asynchrone dans `MainLayout.razor` pour rediriger les utilisateurs selon leur rôle (étudiant ou entreprise).
    - Suppression de la route par défaut dans `Home.razor` et ajout de nouvelles pages pour l'entreprise et l'étudiant.
    - Modification de la méthode de stockage de l'email dans `Login.razor` pour utiliser `SetItemAsStringAsync`.
    - Nettoyage du fichier `StageTracker.Web.csproj` en supprimant des références inutiles.
    - Ajout de styles CSS pour les boutons de soumission dans `app.css`.
    - Création de nouveaux modèles de données : `OfferLiteSearchDto` et `StudentHomeDto`.
    - Ajout d'une nouvelle structure pour la page de l'étudiant dans `Home.razor`, incluant un formulaire de recherche et une section d'affichage des offres.

---
+++ StageTracker.Shared/ModelsDtoWpfWeb/OfferLiteSearchDto.cs
@@ -0,0 +1,7 @@
+﻿namespace StageTracker.Shared.ModelsDtoWpfWeb;
+
+public record class OfferLiteSearchDto
+{
+    public string? SearchTerms { get; set; }
+    public string? Location { get; set; }
+}
---
+++ StageTracker.Shared/ModelsDtoWpfWeb/StudentHomeDto.cs
@@ -0,0 +1,7 @@
+﻿namespace StageTracker.Shared.ModelsDtoWpfWeb;
+
+public record class StudentHomeDto
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
