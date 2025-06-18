6ed500c21ffa19acc371b81d9d77a30a7b40e08a
Frédéric Carisey <63855888+fcarisey@users.noreply.github.com>
2025-06-18 16:36:19 +0200

    Améliore l'affichage par défaut et la sécurité du code
    Ajout de la classe CSS `collapse show` dans `ShowOffer.razor` pour afficher la section par défaut. Utilisation de l'opérateur de coalescence nulle (`?.`) dans `ShowOffer.js` pour éviter les erreurs potentielles lors de l'accès aux classes de `togglerButton`.

--- StageTracker.Web/wwwroot/scripts/ShowOffer.js
+++ StageTracker.Web/wwwroot/scripts/ShowOffer.js
@@ -1,7 +1,6 @@
 ﻿window.onresize = () => {
     let togglerButton = document.getElementsByClassName("offer-application-info-2-toggler")[0];
 
-    if (togglerButton != null)
-        if (togglerButton.classList.contains("collapsed") && window.innerWidth >= 992)
-            togglerButton.click();
+    if (togglerButton?.classList.contains("collapsed") && window.innerWidth >= 992)
+        togglerButton.click();
 }
