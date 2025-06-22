48a45f453ff5a7a72df9abae4725543ca692677e
Frédéric Carisey <63855888+fcarisey@users.noreply.github.com>
2025-06-22 12:14:39 +0200

    Début de création de la page de recherche avancé avec son Dto

---
+++ StageTracker.Shared/ModelsDtoWpfWeb/OfferDetailedSearchDto.cs
@@ -0,0 +1,13 @@
+﻿using StageTracker.Shared.Enums;
+
+namespace StageTracker.Shared.ModelsDtoWpfWeb;
+
+public record class OfferDetailedSearchDto
+{
+    public string? SearchTerms { get; set; }
+    public string? Location { get; set; }
+    public string? Category { get; set; }
+    public EOfferSchedule Schedule { get; set; } = EOfferSchedule.UNASSIGNED;
+    public EOfferAway Away { get; set; } = EOfferAway.UNASSIGNED;
+    public EOfferContractType ContractType { get; set; } = EOfferContractType.UNASSIGNED;
+}
