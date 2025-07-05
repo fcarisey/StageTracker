b1bf42b3f17ea872c70778024395058a1778b92d
Frédéric Carisey <63855888+fcarisey@users.noreply.github.com>
2025-07-05 12:56:27 +0200

    Bootstrap CDN

---
+++ StageTracker.Shared/ModelsDtoWpfWeb/StudentApplyDto.cs
@@ -0,0 +1,11 @@
+﻿using StageTracker.Shared.Enums;
+
+namespace StageTracker.Shared.ModelsDtoWpfWeb;
+
+public record class StudentApplyDto
+{
+    public EApplicationStatus Status { get; set; }
+    public string? MessageToRecruiter { get; set; }
+    public int StudentId { get; set; }
+    public int OfferId { get; set; }
+}
