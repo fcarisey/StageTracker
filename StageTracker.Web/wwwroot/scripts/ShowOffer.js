55954c996ebd122cd12e8a565f7106e9cb13c0a0
Frédéric Carisey <63855888+fcarisey@users.noreply.github.com>
2025-07-05 12:57:12 +0200

    Delete local bootstrap assets

--- StageTracker.Web/wwwroot/scripts/ShowOffer.js
+++ StageTracker.Web/wwwroot/scripts/ShowOffer.js
@@ -1,6 +1,25 @@
 ﻿window.onresize = () => {
-    let togglerButton = document.getElementsByClassName("offer-application-info-2-toggler")[0];
+    const togglerButton = document.getElementsByClassName("offer-application-info-2-toggler")[0];
 
     if (togglerButton?.classList.contains("collapsed") && window.innerWidth >= 992)
         togglerButton.click();
+}
+
+window.initShowOffer = async () => {
+    let togglerButton = document.getElementsByClassName("offer-application-info-2-toggler")[0];
+
+    for (let i = 0; i < 50; i++) {
+        if (togglerButton) {
+            togglerButton = document.getElementsByClassName("offer-application-info-2-toggler")[0];
+
+            togglerButton.addEventListener('onclick', (e) => {
+                button = HTMLElement(e.target);
+
+                button.toggleAttribut('active');
+            });
+
+            break;
+        }
+        await new Promise(res => setTimeout(res, 50));
+    }   
 }
